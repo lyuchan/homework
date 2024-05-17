c77679a9cd43b3215a16db5404251f7207192d15
lyuchan <david12312330@gmail.com>
2024-05-17 10:17:27 +0800

    hi

--- web.js
+++ web.js
@@ -67,11 +67,11 @@ function read() {
                             console.log(`V:${v} A:${a}  W:${w} F:${f}`)
                             send(JSON.stringify({ v: v, i: a, p: w, f: f, sw: sw }))
                         })
-                    }, 10)
+                    }, 20)
                 })
-            }, 10)
-        }, 10)
-    }, 10)
+            }, 20)
+        }, 20)
+    }, 20)
     /*
 */
 }
